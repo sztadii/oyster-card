9241881d4a465936e5df6c43fc494d983798c3c2
Krystian Sz <sztadii@gmail.com>
2022-07-07 03:35:16 +0800

    Add initial test for oyster-card

--- src/oyster-card.test.ts
+++ src/oyster-card.test.ts
@@ -1,8 +1,40 @@
 import { OysterCard } from './oyster-card'
+import { BusService, MetroService } from './services'
 
 describe('OysterCard', () => {
-  it('set the value', () => {
-    const oysterCard = new OysterCard('new value')
-    expect(oysterCard.value).toBe('new value')
+  it('works for the following / requested trip', () => {
+    const oysterCard = new OysterCard(30, new BusService(), new MetroService())
+
+    // 2.5 pounds fare
+    oysterCard.touchTheReader({
+      type: 'metro',
+      station: 'Holborn',
+      gateType: 'entry'
+    })
+    oysterCard.touchTheReader({
+      type: 'metro',
+      station: 'Earl’s Court',
+      gateType: 'exit'
+    })
+
+    // 1.8 pounds fare
+    oysterCard.touchTheReader({
+      type: 'bus',
+      station: 'Earl’s Court'
+    })
+
+    // 2 pounds fare
+    oysterCard.touchTheReader({
+      type: 'metro',
+      station: 'Earl’s Court',
+      gateType: 'entry'
+    })
+    oysterCard.touchTheReader({
+      type: 'metro',
+      station: 'Hammersmith',
+      gateType: 'exit'
+    })
+
+    expect(oysterCard.getBalance()).toBe(30 - 2.5 - 1.8 - 2)
   })
 })
